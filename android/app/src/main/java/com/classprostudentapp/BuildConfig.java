b0d314e598549257222fa2d7dbdf4b82bc24a624
shekhar7860 <shekharshanky20@gmail.com>
2020-07-20 14:35:36 +0530

    first commit

---
+++ android/app/src/main/java/com/classprostudentapp/BuildConfig.java
@@ -0,0 +1,10 @@
+package com.classprostudentapp;
+
+public final class BuildConfig {
+    public static final String APPLICATION_ID = "com.classprostudentapp";
+    public static final String BUILD_TYPE = "release";
+    public static final boolean DEBUG = false;
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 70;
+    public static final String VERSION_NAME = "3.3.0";
+}
